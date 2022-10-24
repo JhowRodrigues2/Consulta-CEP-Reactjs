794740c108a9b3cbf4d69a2505da5631fac40654
JhowRodrigues <jhonatta_rodrigues@hotmail.com>
2022-10-24 20:13:26 -0300

    Error Handling

--- src/Components/BuscaCEP.jsx
+++ src/Components/BuscaCEP.jsx
@@ -3,44 +3,61 @@ import { useState } from "react";
 
 export default function BuscaCep() {
   const [CEP, setCEP] = useState("");
-  const [DATA, setDATA] = useState([]);
+  const [DATA, setDATA] = useState([false]);
+  const [error, setError] = useState(null);
 
-  const searchAddress = () => {
+  const searchAddress = async () => {
     let url = `https://viacep.com.br/ws/${CEP}/json/`;
-    fetch(url)
+
+    await fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setDATA(data);
+        setError(null);
       })
       .catch(() => {
-        alert("Dados Inválido! ");
+        setError("CEP Inválido!");
         setDATA("");
       });
+  };
+  console.log(DATA);
+
+  const handleChange = ({ target }) => {
+    setCEP(target.value);
   };
 
   return (
     <div className="BuscaCep">
       <div className="main">
-        <h1>Informe o CEP:</h1>
+        <h1>Informe o CEP</h1>
 
         <div className="main-top">
           <input
             type="text"
             name="cepCidade"
-            placeholder="Digite o CEP"
-            onChange={(e) => setCEP(e.target.value)}
+            placeholder="00000-000"
+            onChange={handleChange}
+            maxLength={9}
           />
           <button onClick={searchAddress}>Buscar</button>
         </div>
-
+        <p className="error">{error}</p>
         <label>Localidade: </label>
-        <input type="text" value={DATA.logradouro} disabled={true} />
+        <input
+          type="text"
+          value={DATA ? DATA.logradouro : ""}
+          disabled={true}
+        />
         <label>Bairro: </label>
-        <input type="text" value={DATA.bairro} disabled={true} />
+        <input type="text" value={DATA ? DATA.bairro : ""} disabled={true} />
         <label>cidade: </label>
-        <input type="text" value={DATA.localidade} disabled={true} />
+        <input
+          type="text"
+          value={DATA ? DATA.localidade : ""}
+          disabled={true}
+        />
         <label>UF: </label>
-        <input type="text" value={DATA.uf} disabled={true} />
+        <input type="text" value={DATA ? DATA.uf : ""} disabled={true} />
       </div>
     </div>
   );
